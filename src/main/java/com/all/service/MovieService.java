14336fb17fe5efe7d984403bac7e83ac9a8ea56b
Wesley Rosa <wesleypikemen@hotmail.com>
2019-12-19 01:49:41 -0300

    Refactor. clean code

--- src/main/java/com/all/service/MovieService.java
+++ src/main/java/com/all/service/MovieService.java
@@ -1,12 +1,8 @@
 package com.all.service;
 
-import java.util.Optional;
-
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.all.model.Movie;
 import com.all.repository.MovieRepository;
 
 @Service
@@ -15,12 +11,5 @@ public class MovieService {
 
 	@Autowired
 	private MovieRepository movie;
-
-	public Movie find(Integer id) {
-		Optional<Movie> obj = movie.findById(id);
-		//movie.findAll()
-		return obj.orElseThrow(() -> new ObjectNotFoundException(null,
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Movie.class.getName()));
-	}
 
 }
--- src/main/java/com/all/service/StoreService.java
+++ src/main/java/com/all/service/StoreService.java
@@ -1,12 +1,8 @@
 package com.all.service;
 
-import java.util.Optional;
-
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.all.model.Store;
 import com.all.repository.StoreRepository;
 
 @Service
